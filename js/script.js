add08eb7c3f6d172a6163171e974d65917f862b9
Gumadan <fral1488@gmail.com>
2023-07-22 23:48:39 +0400

    add banner section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-	console.log('proverka script.js');
+	
+	const bannerSwiper = new Swiper('.banner-swiper', {
+		speed: 1000,
+		spaceBetween: 0,
+		navigation: {
+			nextEl: '.banner-swiper .swiper-button-next',
+			prevEl: '.banner-swiper .swiper-button-prev',
+		},
+		pagination: {
+			el: '.banner-swiper .swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+	  });
+	  
+	  
 })
