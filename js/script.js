d05f1d3ac58ac8205815c852cdd1f4302c307a8d
Gumadan <fral1488@gmail.com>
2023-07-28 01:26:06 +0400

    add s-why, s-team, s-reviews

--- js/script.js
+++ js/script.js
@@ -1,59 +1,65 @@
-document.addEventListener('DOMContentLoaded', () => {
-	
-	const bannerSwiper = new Swiper('.banner-swiper', {
-		speed: 1000,
-		spaceBetween: 0,
-		navigation: {
-			nextEl: '.banner-swiper .swiper-button-next',
-			prevEl: '.banner-swiper .swiper-button-prev',
-		},
-		pagination: {
-			el: '.banner-swiper .swiper-pagination',
-			type: 'bullets',
-			clickable: true
-		},
-	  });
-	  
-	  $('.services-btn').magnificPopup({
-		type: 'inline'
-	  });
-	  $('.modal-form-close').on('click', function(){
-		$.magnificPopup.close();
-	  });
+document.addEventListener("DOMContentLoaded", () => {
+  const bannerSwiper = new Swiper(".banner-swiper", {
+    speed: 1000,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".banner-swiper .swiper-button-next",
+      prevEl: ".banner-swiper .swiper-button-prev",
+    },
+    pagination: {
+      el: ".banner-swiper .swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
 
-	  $('.gallery-wrap a').magnificPopup({
-		type: 'image',
-		gallery: {
-			enabled: true
-		}
-	  });
+  $(".services-btn").magnificPopup({
+    type: "inline",
+  });
+  $(".modal-form-close").on("click", function () {
+    $.magnificPopup.close();
+  });
 
-	  $('.gallery-btn a').on('click', function(e){
-		
-		e.preventDefault();
-		var galleryItem = $('.gallery-item');
+  $(".gallery-wrap a").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
 
-		if($(this).hasClass('is-active')) {
-			$(this).removeClass('is-active');
-			$(this).text('Показать еще');
-			galleryItem.each(function () {
-				if($(this).hasClass('is-active')){
-					$(this).removeClass('is-active');
-					$(this).slideUp();
-				}
-			});
-		}
-		else {
-			$(this).addClass('is-active');
-			$(this).text('Скрыть');
-			galleryItem.each(function () {
-				if(!$(this).is(':visible')) {
-					$(this).addClass('is-active');
-					$(this).slideDown();
-				}
-			});
-		}
+  $(".gallery-btn a").on("click", function (e) {
+    e.preventDefault();
+    var galleryItem = $(".gallery-item");
 
-	  });
-	  
-})
+    if ($(this).hasClass("is-active")) {
+      $(this).removeClass("is-active");
+      $(this).text("Показать еще");
+      galleryItem.each(function () {
+        if ($(this).hasClass("is-active")) {
+          $(this).removeClass("is-active");
+          $(this).slideUp();
+        }
+      });
+    } else {
+      $(this).addClass("is-active");
+      $(this).text("Скрыть");
+      galleryItem.each(function () {
+        if (!$(this).is(":visible")) {
+          $(this).addClass("is-active");
+          $(this).slideDown();
+        }
+      });
+    }
+  });
+
+  const reviewsSwiper = new Swiper(".reviews-swiper", {
+    speed: 1000,
+    spaceBetween: 25,
+    slidesPerView: 3,
+    pagination: {
+      el: ".reviews-swiper .swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+});
